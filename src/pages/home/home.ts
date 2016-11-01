fbeca5e4c514a859758cbda3e32a6d3948c080d9
Tassos Lambrou <lambrou@gmail.com>
2016-11-01 01:13:51 -0700

    Some minor changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, alerCtrl: AlertController) {
 
   }
+
+
+ doConfirm() {
+   let confirm = this.alerCtrl.create({
+     title: 'Turn on Your Location?',
+     message: 'Do you agree to use your location to use this app?',
+     buttons: [
+       {
+         text: 'Disagree',
+         handler: () => {
+           console.log('Disagree clicked');
+         }
+       },
+       {
+         text: 'Agree',
+         handler: () => {
+           console.log('Agree clicked');
+         }
+       }
+     ]
+   });
+   confirm.present()
+ }
 
 }
