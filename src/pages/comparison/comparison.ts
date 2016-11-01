70ab86239a5defa4747487bc14a6d493a69d602d
Tassos Lambrou <lambrou@gmail.com>
2016-11-01 14:06:51 -0700

    Modified Comparison view

--- src/pages/comparison/comparison.ts
+++ src/pages/comparison/comparison.ts
@@ -13,29 +13,96 @@ import { NavController } from 'ionic-angular';
 })
 export class ComparisonPage {
 
-
-// Doughnut
-public doughnutChartLabels:string[] = ['Transparency', 'Elections', 'Public Safety', 'Health and Social Services', 'Economy', 'Administration', 'Transportation', 'Waste Management'];
-public doughnutChartData:number[] = [73, 78, 95, 63, 73, 62, 91, 36];
-public doughnutChartType:string = 'doughnut';
-
 // chart data
 public lineChartData:Array<any> = [
-  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Category A'},
-  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Category B'},
-  {data: [18, 48, 77, 9, 100, 27, 40], label: 'Category C'}
+  {data: [65, 59, 80, 81, 56, 55, 48], label: 'San Francisco'},
+  {data: [38, 41, 43, 56, 50, 51, 64], label: 'Seattle'}
 ];
-public lineChartLabels:Array<any> = ['Jul', 'Aug', 'Sep', 'Oct'];
+public lineChartLabels:Array<any> = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan'];
 public lineChartType:string = 'line';
+public lineChartLegend:boolean = false;
 
-public doughnutChartColors:Array<any> = [
-{ // Red
-  backgroundColor: ['rgba(196,77,88,0.8)', 'rgba(245,190,53,0.8)', 'rgba(199,244,100,0.8)', 'rgba(78,205,196,0.8)', 'rgba(22,193,243,0.8)', 'rgba(85,98,112,0.8)', 'rgba(145,32,77,0.8)', 'rgba(228,132,74,0.8)'],
-  borderColor:  ['rgba(196,77,88,1)', 'rgba(245,190,53,1)', 'rgba(199,244,100,1)', 'rgba(78,205,196,1)', 'rgba(22,193,243,1)', 'rgba(85,98,112,1)', 'rgba(145,32,77,1)', 'rgba(228,132,74,1)'],
-  pointBackgroundColor:  ['rgba(196,77,88,1)', 'rgba(245,190,53,1)', 'rgba(199,244,100,1)', 'rgba(78,205,196,1)', 'rgba(22,193,243,1)', 'rgba(85,98,112,1)', 'rgba(145,32,77,1)', 'rgba(228,132,74,1)'],
-  pointHoverBorderColor: ['rgba(196,77,88,0.8)', 'rgba(245,190,53,0.8)', 'rgba(199,244,100,0.8)', 'rgba(78,205,196,0.8)', 'rgba(22,193,243,0.8)', 'rgba(85,98,112,0.8)', 'rgba(145,32,77,0.8)', 'rgba(228,132,74,0.8)']
-}];
+public lineChartOptions:any = {
+  scaleShowVerticalLines: false,
+  scales: {
+    xAxes: [{
+      display: false
+    }],
+    yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+      }]
+  },
+  responsive: true
+};
 
+public lineChartColors:Array<any> = [
+  { // coral
+    backgroundColor: 'rgba(196,77,88,0.2)',
+    borderColor: 'rgba(196,77,88,1)',
+    pointBackgroundColor: 'rgba(196,77,88,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(196,77,88,0.8)'
+  },
+  { // aqua
+    backgroundColor: 'rgba(78,205,196,0.2)',
+    borderColor: 'rgba(78,205,196,1)',
+    pointBackgroundColor: 'rgba(78,205,196,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(78,205,196,0.8)'
+  },
+  { // green
+    backgroundColor: 'rgba(199,244,100,0.2)',
+    borderColor: 'rgba(199,244,100,1)',
+    pointBackgroundColor: 'rgba(199,244,100,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(199,244,100,0.8)'
+  },
+  { // blue
+    backgroundColor: 'rgba(22,193,243,0.2)',
+    borderColor: 'rgba(22,193,243,1)',
+    pointBackgroundColor: 'rgba(22,193,243,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(22,193,243,0.8)'
+  },
+  { // grey
+    backgroundColor: 'rgba(85,98,112,0.2)',
+    borderColor: 'rgba(85,98,112,1)',
+    pointBackgroundColor: 'rgba(85,98,112,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(85,98,112,0.8)'
+  },
+  { // yellow
+    backgroundColor: 'rgba(245,190,53,0.2)',
+    borderColor: 'rgba(245,190,53,1)',
+    pointBackgroundColor: 'rgba(245,190,53,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(245,190,53,1)'
+  },
+  { // red
+    backgroundColor: 'rgba(145,32,77,0.2)',
+    borderColor: 'rgba(145,32,77,1)',
+    pointBackgroundColor: 'rgba(145,32,77,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(145,32,77,0.8)'
+  },
+  { // orange
+    backgroundColor: 'rgba(228,132,74,0.2)',
+    borderColor: 'rgba(228,132,74,1)',
+    pointBackgroundColor: 'rgba(228,132,74,1)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgba(228,132,74,0.8)'
+  }
+];
   constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
